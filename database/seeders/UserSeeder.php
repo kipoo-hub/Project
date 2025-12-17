0d7c7340f27fd872990a5d02debb8c0d43accab8
kipoo-hub <taufiq24si@mahasiswa.pcr.ac.id>
2025-12-17 11:01:04 +0700

    Perbaikan seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,7 +10,7 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
+        User::updateOrCreate([
             'name'     => 'Administrator',
             'email'    => 'admin1@example.com',
             'password' => Hash::make('password'),
